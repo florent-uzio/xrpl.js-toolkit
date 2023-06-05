d8e58a81f0b2eb72931979617c61e7771930db38
Florent <36513774+florent-uzio@users.noreply.github.com>
2023-06-05 19:37:01 +0400

    cancel offer

--- src/methods/path-and-order-book/book-offers.ts
+++ src/methods/path-and-order-book/book-offers.ts
@@ -1,4 +1,4 @@
-import { BookOffersRequest, xrpToDrops } from "xrpl"
+import { BookOffersRequest } from "xrpl"
 import { convertCurrencyCodeToHex } from "../../helpers"
 import { getXrplClient } from "../../xrpl-client"
 
@@ -10,15 +10,6 @@ export const getBookOffers = async ({ taker_gets, taker_pays, ...rest }: BookOff
   // Convert currencies to hex if needed
   taker_gets.currency = convertCurrencyCodeToHex(taker_gets.currency)
   taker_pays.currency = convertCurrencyCodeToHex(taker_pays.currency)
-
-  // Convert XRP to drops
-  if (typeof taker_gets.currency === "string") {
-    taker_gets.currency = xrpToDrops(taker_gets.currency)
-  }
-
-  if (typeof taker_pays.currency === "string") {
-    taker_pays.currency = xrpToDrops(taker_pays.currency)
-  }
 
   // Send the request
   const response = await getXrplClient().request({

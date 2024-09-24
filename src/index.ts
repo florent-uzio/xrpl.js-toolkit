9d9929ade30880d89deb510586045bc385154129
Florent Uzio <fuzio@ripple.com>
2024-09-24 10:22:31 +0400

    removed trustline, updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -14,20 +14,7 @@ const main = async () => {
   // Do not comment
   await client.connect()
 
-  /**
-   *   ___  _   _
-   *  / _ \| |_| |__   ___ _ __ ___
-   * | | | | __| '_ \ / _ \ '__/ __|
-   * | |_| | |_| | | |  __/ |  \__ \
-   *  \___/ \__|_| |_|\___|_|  |___/
-   */
-
-  // await showAccountBalanceChanges(WALLET_1.address, client)
-
-  // await showTxBalanceChanges(
-  //   "8821A8EF3E1BC04B59FC2C4056EDC6C8440BF6E40B231D810936C159953A44E4",
-  //   client,
-  // )
+  // Write your code here
 
   // Do not comment, disconnect the client
   await client.disconnect()

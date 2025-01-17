c69c0bd8579c76aaace156d1d417ea75fe90700d
Florent Uzio <fuzio@ripple.com>
2025-01-17 16:49:52 +0400

    accountSet working, needs improvements

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import * as dotenv from "dotenv"
-import { Client } from "xrpl"
-import { submitMethod } from "./methods"
+import { AccountSetAsfFlags, Client } from "xrpl"
 import { networks } from "./networks"
-import { WALLET_1 } from "./wallets"
+import { issueTokenTasks } from "./tasks"
 
 dotenv.config()
 
@@ -11,19 +10,45 @@ dotenv.config()
 const TOKEN = process.env.TOKEN ?? "TEST_TOKEN"
 
 const main = async () => {
-  const client = new Client(networks.testnet.ripple)
+  const client = new Client(networks.devnet.ripple)
 
   // Do not comment
   await client.connect()
 
-  // Write your code here...
-  await submitMethod({
-    request: {
-      command: "account_info",
-      account: WALLET_1.classicAddress,
+  await issueTokenTasks({
+    network: networks.devnet.ripple,
+    numOperationalAccounts: 2,
+    numHolderAccounts: 3,
+    fundingOptions: { amount: "75" },
+    issuerSettings: {
+      Domain: "https://test.flo.com",
+      TickSize: 6,
+      TransferRate: 1400000000,
+      setFlags: [
+        AccountSetAsfFlags.asfRequireAuth,
+        AccountSetAsfFlags.asfDefaultRipple,
+        AccountSetAsfFlags.asfDepositAuth,
+      ],
     },
-    client,
   })
+
+  // await submitTxnAndWait({
+  //   txn: {
+  //     Account: WALLET_1.address,
+  //     TransactionType: "TicketCreate",
+  //     TicketCount: 10,
+  //   },
+  //   client,
+  //   wallet: WALLET_1,
+  // })
+
+  // await submitMethod({
+  //   request: {
+  //     command: "account_info",
+  //     account: "r3UZSQkjJMt5JyZuEZwVRdfHUofPH7bzK8",
+  //   },
+  //   client,
+  // })
 
   // Do not comment, disconnect the client
   await client.disconnect()
--- src/tasks/issue-token.ts
+++ src/tasks/issue-token.ts
@@ -1,8 +1,18 @@
 import { writeFileSync } from "fs"
 import { delay, Listr } from "listr2"
 import path from "path"
-import { Client, FundingOptions, Wallet } from "xrpl"
+import {
+  AccountSet,
+  AccountSetAsfFlags,
+  Client,
+  convertStringToHex,
+  FundingOptions,
+  Wallet,
+} from "xrpl"
+import { Ticket } from "xrpl/dist/npm/models/ledger"
 import { isUndefined } from "../helpers"
+import { submitMethod } from "../methods"
+import { submitTxnAndWait } from "../transactions"
 import { IssueTokenContext } from "./issue-token.types"
 
 type IssueTokenProps = {
@@ -10,6 +20,9 @@ type IssueTokenProps = {
   numOperationalAccounts?: number
   numHolderAccounts?: number
   fundingOptions?: FundingOptions
+  issuerSettings?: {
+    setFlags?: AccountSetAsfFlags[]
+  } & Pick<AccountSet, "Domain" | "TickSize" | "TransferRate" | "ClearFlag">
 }
 
 /**
@@ -29,7 +42,8 @@ export const issueTokenTasks = async (props: IssueTokenProps) => {
       ;(ctx.client = new Client(props.network)),
         (ctx.issuer = Wallet.generate()),
         (ctx.operationals = []),
-        (ctx.holders = [])
+        (ctx.holders = []),
+        (ctx.issuerTickets = [])
     },
   })
 
@@ -59,6 +73,58 @@ export const issueTokenTasks = async (props: IssueTokenProps) => {
   })
 
   tasks.add({
+    title: "Creating tickets for the issuer",
+    enabled: canCreateTicketsForIssuer(props.issuerSettings),
+    task: async (ctx, task) => {
+      const numOfTicketsToCreate = calculateIssuerSettings(props.issuerSettings)
+
+      if (numOfTicketsToCreate === 0) return
+
+      await submitTxnAndWait({
+        txn: {
+          Account: ctx.issuer.address,
+          TransactionType: "TicketCreate",
+          TicketCount: numOfTicketsToCreate,
+        },
+        wallet: ctx.issuer,
+        client: ctx.client,
+        showLogs: false,
+      })
+
+      delay(1000)
+
+      // Retrieve the ticket objects
+      const tickets = await submitMethod({
+        request: {
+          command: "account_objects",
+          type: "ticket",
+          account: ctx.issuer.address,
+        },
+        client: ctx.client,
+        showLogs: false,
+      })
+
+      ctx.issuerTickets = tickets.result.account_objects as Ticket[]
+
+      task.output = `Created ${numOfTicketsToCreate} tickets for the issuer`
+    },
+  })
+
+  tasks.add({
+    title: "Configuring the issuer",
+    task: async (ctx, task) => {
+      const subtasks = task.newListr<IssueTokenContext>([], {
+        concurrent: canCreateTicketsForIssuer(props.issuerSettings),
+        rendererOptions: { collapseSubtasks: false },
+      })
+      //   task.output = ctx.issuerTickets
+      configureIssuer({ ctx, tasks: subtasks, issuerSettings: props.issuerSettings })
+
+      return subtasks
+    },
+  })
+
+  tasks.add({
     title: "Disconnect the client",
     task: async (ctx) => {
       await ctx.client.disconnect()
@@ -66,7 +132,7 @@ export const issueTokenTasks = async (props: IssueTokenProps) => {
   })
 
   tasks.add({
-    title: "Writing results to a file",
+    title: "Writing results to a file in the src/tasks/output directory",
     task: async (ctx) => {
       const time = new Date().toISOString()
       const pathFile = path.join(__dirname, "./output/", `results-${time}.json`)
@@ -108,7 +174,6 @@ const createWallets = ({
     title: "Creating operational account(s)",
     enabled: () => !isUndefined(numOperationalAccounts) && numOperationalAccounts > 0,
     task: async (ctx) => {
-      delay(random(3, 6))
       const operationalAccounts = Array.from({ length: numOperationalAccounts ?? 0 }, async () => {
         const operationalAccount = await ctx.client.fundWallet(null, fundingOptions)
         return operationalAccount.wallet
@@ -122,8 +187,8 @@ const createWallets = ({
     title: "Creating holder account(s)",
     enabled: () => !isUndefined(numHolderAccounts) && numHolderAccounts > 0,
     task: async (ctx) => {
-      delay(random(3, 6))
       const holderAccounts = Array.from({ length: numHolderAccounts ?? 0 }, async () => {
+        // delay(random(3, 6))
         const account = await ctx.client.fundWallet(null, fundingOptions)
         return account.wallet
       })
@@ -136,3 +201,222 @@ const createWallets = ({
 }
 
 const random = (min: number, max: number) => Math.round((Math.random() * (max - min) + min) * 1000)
+
+type ConfigureIssuerProps = {
+  ctx: IssueTokenContext
+  tasks: Listr<IssueTokenContext>
+} & Pick<IssueTokenProps, "issuerSettings">
+
+export const configureIssuer = async ({ tasks, issuerSettings }: ConfigureIssuerProps) => {
+  tasks.add({
+    title: "Setting Domain",
+    enabled: () =>
+      !isUndefined(issuerSettings?.Domain) ||
+      !isUndefined(issuerSettings?.TickSize) ||
+      !isUndefined(issuerSettings?.TransferRate),
+    task: async (ctx) => {
+      const txn: AccountSet = {
+        Account: ctx.issuer.address,
+        TransactionType: "AccountSet",
+        Domain: convertStringToHex(issuerSettings?.Domain ?? ""),
+        TickSize: issuerSettings?.TickSize,
+        TransferRate: issuerSettings?.TransferRate,
+      }
+
+      if (canCreateTicketsForIssuer(issuerSettings)) {
+        const ticket = ctx.issuerTickets.shift()
+        if (!ticket) {
+          throw new Error("No available tickets for setting Domain")
+        }
+        txn.TicketSequence = ticket.TicketSequence
+        txn.Sequence = 0
+      }
+
+      await submitTxnAndWait({
+        txn,
+        wallet: ctx.issuer,
+        client: ctx.client,
+        showLogs: false,
+      })
+    },
+    retry: 1,
+  })
+
+  //   tasks.add({
+  //     title: "Setting TickSize",
+  //     enabled: () => !isUndefined(issuerSettings?.TickSize),
+  //     task: async (ctx) => {
+  //       delay(random(1, 4))
+  //       const ticket = ctx.issuerTickets.shift()
+  //       if (!ticket) throw new Error("No available tickets for setting TickSize")
+
+  //       await submitTxnAndWait({
+  //         txn: {
+  //           Account: ctx.issuer.address,
+  //           TransactionType: "AccountSet",
+  //           TickSize: issuerSettings?.TickSize,
+  //           TicketSequence: ticket.TicketSequence,
+  //           Sequence: 0,
+  //         },
+  //         wallet: ctx.issuer,
+  //         client: ctx.client,
+  //         showLogs: false,
+  //       })
+  //     },
+  //   })
+
+  //   tasks.add({
+  //     title: "Setting TransferRate",
+  //     enabled: () => !isUndefined(issuerSettings?.TransferRate),
+  //     task: async (ctx) => {
+  //       delay(random(1, 4))
+  //       const ticket = ctx.issuerTickets.shift()
+  //       if (!ticket) throw new Error("No available tickets for setting TransferRate")
+
+  //       await submitTxnAndWait({
+  //         txn: {
+  //           Account: ctx.issuer.address,
+  //           TransactionType: "AccountSet",
+  //           TransferRate: issuerSettings?.TransferRate,
+  //           TicketSequence: ticket.TicketSequence,
+  //           Sequence: 0,
+  //         },
+  //         wallet: ctx.issuer,
+  //         client: ctx.client,
+  //         showLogs: false,
+  //       })
+  //     },
+  //   })
+
+  tasks.add({
+    title: "Setting AccountSet flags",
+    enabled: () => !isUndefined(issuerSettings?.setFlags),
+    task: async (_, task) => {
+      if (!issuerSettings?.setFlags) return
+
+      const subtasks = task.newListr<IssueTokenContext>([], { concurrent: false })
+
+      for (const flag of issuerSettings.setFlags) {
+        subtasks.add({
+          title: `Setting flag number: ${flag}`,
+          task: async (ctx) => {
+            // delay(random(1, 4))
+            // const ticket = ctx.issuerTickets.shift()
+            // if (!ticket) {
+            //   throw new Error(`No available tickets for setting AccountSet flags number: ${flag}`)
+            // }
+
+            await submitTxnAndWait({
+              txn: {
+                Account: ctx.issuer.address,
+                TransactionType: "AccountSet",
+                SetFlag: flag,
+                // TicketSequence: ticket.TicketSequence,
+                // Sequence: 0,
+              },
+              wallet: ctx.issuer,
+              client: ctx.client,
+              showLogs: false,
+            })
+          },
+        })
+      }
+
+      return subtasks
+    },
+
+    //   await Promise.all(
+    //     issuerSettings.setFlags.map(async (flag) => {
+    //       delay(random(1, 4))
+
+    //       const txn: AccountSet = {
+    //         Account: ctx.issuer.address,
+    //         TransactionType: "AccountSet",
+    //         SetFlag: flag,
+    //       }
+
+    //       if (canCreateTicketsForIssuer(issuerSettings)) {
+    //         const ticket = ctx.issuerTickets.shift()
+    //         if (!ticket) {
+    //           throw new Error(`No available tickets for setting AccountSet flags number: ${flag}`)
+    //         }
+    //         txn.TicketSequence = ticket.TicketSequence
+    //         txn.Sequence = 0
+    //       }
+
+    //       await submitTxnAndWait({
+    //         txn,
+    //         wallet: ctx.issuer,
+    //         client: ctx.client,
+    //         showLogs: false,
+    //       })
+    //     }),
+    //   )
+    // },
+    // retry: 1,
+  })
+
+  tasks.add({
+    title: "Clearing AccountSet flags",
+    enabled: () => !isUndefined(issuerSettings?.ClearFlag),
+    task: async (ctx) => {
+      const flags = Array.isArray(issuerSettings?.ClearFlag)
+        ? issuerSettings?.ClearFlag
+        : [issuerSettings?.ClearFlag]
+
+      await Promise.all(
+        flags.map(async (flag) => {
+          delay(random(1, 4))
+          const ticket = ctx.issuerTickets.shift()
+          if (!ticket) {
+            throw new Error(`No available tickets for clearing AccountSet flags number: ${flag}`)
+          }
+
+          await submitTxnAndWait({
+            txn: {
+              Account: ctx.issuer.address,
+              TransactionType: "AccountSet",
+              ClearFlag: flag,
+              TicketSequence: ticket.TicketSequence,
+              Sequence: 0,
+            },
+            wallet: ctx.issuer,
+            client: ctx.client,
+            showLogs: false,
+          })
+        }),
+      )
+    },
+  })
+
+  return tasks
+}
+
+const calculateIssuerSettings = (issuerSettings: IssueTokenProps["issuerSettings"]) => {
+  let totalSettings = 0
+
+  const { Domain, TickSize, TransferRate, setFlags, ClearFlag } = issuerSettings ?? {}
+
+  if (Domain || TickSize || TransferRate) totalSettings++
+
+  if (!isUndefined(issuerSettings?.setFlags)) {
+    const setFlags =
+      !isUndefined(issuerSettings?.setFlags) && Array.isArray(issuerSettings?.setFlags)
+        ? issuerSettings?.setFlags
+        : [issuerSettings?.setFlags]
+    totalSettings = totalSettings + setFlags.length
+  }
+
+  if (!isUndefined(issuerSettings?.ClearFlag)) {
+    const clearFlags = Array.isArray(issuerSettings?.ClearFlag)
+      ? issuerSettings?.ClearFlag
+      : [issuerSettings?.ClearFlag]
+    totalSettings = totalSettings + clearFlags.length
+  }
+
+  return totalSettings
+}
+
+const canCreateTicketsForIssuer = (issuerSettings: IssueTokenProps["issuerSettings"]) => {
+  return !issuerSettings?.setFlags?.includes(AccountSetAsfFlags.asfRequireAuth)
+}

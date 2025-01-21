6a8563df085250fe573f6d7ac0e4140c8e91c1bd
Florent Uzio <fuzio@ripple.com>
2025-01-21 16:44:56 +0400

    upgraded xrpl version to 4.1.0

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import * as dotenv from "dotenv"
-import { AccountSetAsfFlags, Client, TrustSetFlags } from "xrpl"
+import { Client } from "xrpl"
 import { networks } from "./networks"
-import { issueTokenTasks } from "./tasks"
 // import walletsData from "./tasks/issue-token/output/results-2025-01-21T08:17:56.577Z.json"
 dotenv.config()
 
@@ -17,46 +16,7 @@ const main = async () => {
   // Do not comment
   await client.connect()
 
-  await issueTokenTasks({
-    network: networks.devnet.ripple,
-    operationalAccountCount: 2,
-    holderAccountCount: 2,
-    fundingOptions: { amount: "50" },
-    issuerSettings: {
-      // Domain: "https://test.flo.com",
-      // TickSize: 6,
-      // TransferRate: 1400000000,
-      setFlags: [
-        AccountSetAsfFlags.asfRequireAuth,
-        AccountSetAsfFlags.asfDefaultRipple,
-        // AccountSetAsfFlags.asfDepositAuth,
-      ],
-    },
-    trustLineParams: {
-      currency: TOKEN,
-      value: "200000000",
-      Flags: TrustSetFlags.tfSetNoRipple,
-    },
-  })
-
-  // await submitTxnAndWait({
-  //   txn: {
-  //     Account: issuer.address,
-  //     TransactionType: "AccountSet",
-  //     ClearFlag: AccountSetAsfFlags.asfRequireAuth,
-  //   },
-  //   client,
-  //   wallet: issuer,
-  // })
-
-  // await submitMethod({
-  //   request: {
-  //     command: "account_info",
-  //     account: "r9PWipZ2RnksPYQNEmaRdiSfxo7jQDs1hW",
-  //     api_version: 2,
-  //   },
-  //   client,
-  // })
+  // Write your code here
 
   // Do not comment, disconnect the client
   await client.disconnect()

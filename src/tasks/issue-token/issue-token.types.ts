a85d68801e708a9c10e2455afcba74b1e98a0d48
Florent Uzio <fuzio@ripple.com>
2025-03-14 18:29:33 +0400

    issue token types in progress

--- src/tasks/issue-token/issue-token.types.ts
+++ src/tasks/issue-token/issue-token.types.ts
@@ -2,6 +2,7 @@ import {
   AccountSet,
   AccountSetAsfFlags,
   Client,
+  CredentialCreate,
   FundingOptions,
   IssuedCurrencyAmount,
   TrustSet,
@@ -64,4 +65,101 @@ export type TokenIssuanceConfig = {
 
   /** If disabled, the script will not run */
   run?: boolean
+}
+
+type Credential = {
+  subject: Recipient["id"]
+} & Pick<CredentialCreate, "CredentialType" | "URI" | "Expiration">
+
+type AccountSettings = {
+  accountSetAsfFlags?: AccountSetAsfFlags[]
+} & Pick<
+  AccountSet,
+  "Domain" | "TickSize" | "TransferRate" | "NFTokenMinter" | "EmailHash" | "MessageKey"
+>
+
+type RequireAuthIssuer = {
+  id: number
+  credentials: Credential[]
+  accountSetAsfFlags: (AccountSetAsfFlags.asfRequireAuth | AccountSetAsfFlags)[]
+  trustLineAuthorize: Recipient["id"][]
+} & Omit<AccountSettings, "accountSetAsfFlags">
+
+type NonRequireAuthIssuer = {
+  id: number
+  credentials: Credential[]
+  accountSetAsfFlags?: Exclude<AccountSetAsfFlags, AccountSetAsfFlags.asfRequireAuth>[]
+  trustLineAuthorize?: never
+} & Omit<AccountSettings, "accountSetAsfFlags">
+
+type Issuer = RequireAuthIssuer | NonRequireAuthIssuer
+
+type Recipient = {
+  id: number
+  accountSettings?: {
+    accountSetAsfFlags?: AccountSetAsfFlags[]
+  } & Pick<AccountSet, "Domain" | "EmailHash" | "MessageKey">
+  trustLineSettings: {
+    currency: IssuedCurrencyAmount["currency"]
+    value: IssuedCurrencyAmount["value"]
+    // reference to the issuer
+    issuerId: Issuer["id"]
+  }
+}
+
+export type TokenIssuanceConfig2 = {
+  /** XRP Ledger network identifier (e.g., "mainnet", "testnet", etc.) */
+  network: string
+
+  /** Number of operational accounts to create (optional) */
+  // operationalAccountCount?: number
+
+  /** Number of issuer accounts to create */
+  issuerAccountCount: number
+
+  /** Number of recipient accounts to create  */
+  recipientAccountCount: number
+
+  /** Options for funding operational and holder accounts */
+  fundingOptions?: FundingOptions
+
+  /** Issuer accounts to create and their settings */
+  issuers: Issuer[]
+
+  recipients: Recipient[]
+
+  /** If disabled, the script will not run */
+  run?: boolean
+}
+
+const test: TokenIssuanceConfig2 = {
+  network: "mainnet",
+  issuerAccountCount: 1,
+  recipientAccountCount: 1,
+  issuers: [
+    {
+      id: 1,
+      credentials: [
+        {
+          subject: 1,
+          CredentialType: "test-creds",
+          URI: "https://example.com",
+          // Expiration: "2025-01-01"
+        },
+      ],
+
+      accountSetAsfFlags: [AccountSetAsfFlags.asfRequireAuth, AccountSetAsfFlags.asfDefaultRipple],
+      trustLineAuthorize: [1],
+    },
+  ],
+  recipients: [
+    {
+      id: 1,
+      trustLineSettings: {
+        currency: "USD",
+        value: "100",
+        issuerId: 1,
+      },
+    },
+  ],
 }
